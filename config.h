1c0826973ccea25348fbdc954f438cc40b959412
nuxsh <nuxshdev@outlook.com>
2021-07-25 17:16:57 +0530

    fix dmenu colors

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_red, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_fg, "-sb", col_red, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {
