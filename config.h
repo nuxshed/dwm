56b70c9e0e09817d559a1f0c2805ac03c51a2fa5
nuxsh <nuxshdev@outlook.com>
2021-07-25 17:15:25 +0530

    change colors and reduce snap size

--- config.h
+++ config.h
@@ -2,20 +2,24 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        // border pixel of windows
-static const unsigned int snap      = 32;       // snap pixel
+static const unsigned int snap      = 10;       // snap pixel
 static const int showbar            = 1;        // 0 means no bar
 static const int topbar             = 1;        // 0 means bottom bar
 static const char *fonts[]          = { "monospace:size=9" };
 static const char dmenufont[]       = "monospace:size=9";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#131519";
+static const char col_gray2[]       = "#1b1d23";
+static const char col_gray3[]       = "#282c34";
+static const char col_fg[]          = "#abb2bf";
+static const char col_magenta[]     = "#c678dd";
+static const char col_red[]         = "#e06c75";
+static const char col_green[]       = "#98c379";
+static const char col_blue[]        = "#61afef";
+static const char col_yellow[]      = "#e5c07b";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	//               fg            bg              border
+	[SchemeNorm] = { col_fg,       col_gray1,      col_gray2 },
+	[SchemeSel]  = { col_gray2,    col_magenta,    col_blue},
 };
 
 /* tagging */
@@ -59,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_red, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {
