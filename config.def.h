77e3bc67b5aff68d97d9b3edfd700386c4d92f1b
nuxsh <nuxshdev@outlook.com>
2021-08-01 12:57:01 +0530

    config.h -> config.def.h

--- config.def.h
+++ config.def.h
@@ -1,26 +1,30 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const unsigned int borderpx  = 2;        // border pixel of windows
+static const unsigned int snap      = 10;       // snap pixel
+static const unsigned int gappih    = 20;       // horiz inner gap between windows
+static const unsigned int gappiv    = 10;       // vert inner gap between windows
+static const unsigned int gappoh    = 10;       // horiz outer gap between windows and screen edge
+static const unsigned int gappov    = 30;       // vert outer gap between windows and screen edge
+static       int smartgaps          = 0;        // 1 means no outer gap when there is only one window
+static const int showbar            = 1;        // 0 means no bar
+static const int topbar             = 1;        // 0 means bottom bar
+static const char *fonts[]          = { "Fira Code Nerd Font:size=8" };
+static const char dmenufont[]       = "Fira Code Nerd Font:size=8";
+static const char col_gray1[]       = "#131519";
+static const char col_gray2[]       = "#1b1d23";
+static const char col_gray3[]       = "#282c34";
+static const char col_fg[]          = "#abb2bf";
+static const char col_magenta[]     = "#c678dd";
+static const char col_red[]         = "#e06c75";
+static const char col_green[]       = "#98c379";
+static const char col_blue[]        = "#61afef";
+static const char col_yellow[]      = "#e5c07b";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	//               fg            bg              border
+	[SchemeNorm] = { col_fg,       col_gray1,      col_gray2 },
+	[SchemeSel]  = { col_gray2,    col_magenta,    col_blue},
 };
 
 /* tagging */
@@ -56,7 +60,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -68,13 +72,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_fg, "-sb", col_red, "-sf", col_gray1, NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -127,6 +132,21 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+        { 0,                            0x1008FF02,spawn,          SHCMD("brightnessctl s +5%")  },
+        { 0,                            0x1008FF03,spawn,          SHCMD("brightnessctl s 5%-")  },
+        { ControlMask,                  0x1008FF02,spawn,          SHCMD("brightnessctl s +1%")  },
+        { ControlMask,                  0x1008FF03,spawn,          SHCMD("brightnessctl s 1%-")  },
+        { 0,                            0x1008FF11,spawn,          SHCMD("vol -5%")  },
+        { 0,                            0x1008FF13,spawn,          SHCMD("vol +5%")  },
+        { ControlMask,                  0x1008FF11,spawn,          SHCMD("vol -1%")  },
+        { ControlMask,                  0x1008FF13,spawn,          SHCMD("vol +1%")  },
+        { 0,                            XK_Print,  spawn,          SHCMD("scr screen")},
+        { ControlMask,                  XK_Print,  spawn,          SHCMD("scr window")},
+        { ShiftMask,                    XK_Print,  spawn,          SHCMD("scr selection")},
+        { MODKEY,                       XK_Print,  spawn,          SHCMD("scr screentoclip")},
+        { MODKEY|ControlMask,           XK_Print,  spawn,          SHCMD("scr windowtoclip")},
+        { MODKEY|ShiftMask,             XK_Print,  spawn,          SHCMD("scr selectiontoclip")},
+        { MODKEY,                       XK_r,      spawn,          SHCMD("rofi -show drun")},
 };
 
 /* button definitions */
