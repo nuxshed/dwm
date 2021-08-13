eda2bb01b5e9e92d833eb4f0ecedcbfd63aa66c9
nuxsh <nuxshed@gmail.com>
2021-08-13 10:48:13 +0530

    fix gaps

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        // border pixel of windows
 static const unsigned int snap      = 10;       // snap pixel
-static const unsigned int gappih    = 20;       // horiz inner gap between windows
+static const unsigned int gappih    = 10;       // horiz inner gap between windows
 static const unsigned int gappiv    = 10;       // vert inner gap between windows
-static const unsigned int gappoh    = 10;       // horiz outer gap between windows and screen edge
-static const unsigned int gappov    = 30;       // vert outer gap between windows and screen edge
+static const unsigned int gappoh    = 20;       // horiz outer gap between windows and screen edge
+static const unsigned int gappov    = 20;       // vert outer gap between windows and screen edge
 static       int smartgaps          = 0;        // 1 means no outer gap when there is only one window
 static const int showbar            = 1;        // 0 means no bar
 static const int topbar             = 1;        // 0 means bottom bar
