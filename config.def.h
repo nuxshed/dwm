6b666c99f814670ac03e40a7f2da18e2a797e992
nuxsh <nuxshed@gmail.com>
2021-08-15 13:19:12 +0530

    update #include in config.def.h

--- config.def.h
+++ config.def.h
@@ -63,7 +63,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
-#include "vanitygaps.c"
+#include "lib/layouts.c"
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
