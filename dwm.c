582d1e2abe03a6d4e1c2de19094e0d3c2282b69e
nuxsh <nuxshed@gmail.com>
2021-08-20 00:32:54 +0530

    move riodraw stuff to lib/rio.c

--- dwm.c
+++ dwm.c
@@ -251,10 +251,6 @@ static void resize(Client *c, int x, int y, int w, int h, int interact);
 static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
-static int riodraw(Client *c, const char slopstyle[]);
-static void rioposition(Client *c, int x, int y, int w, int h);
-static void rioresize(const Arg *arg);
-static void riospawn(const Arg *arg);
 static void run(void);
 static void runAutostart(void);
 static void scan(void);
@@ -322,8 +318,6 @@ static int bh, blw = 0;      /* bar geometry */
 static int lrpad;            /* sum of left and right padding for text */
 static int (*xerrorxlib)(Display *, XErrorEvent *);
 static unsigned int numlockmask = 0;
-static int riodimensions[4] = { -1, -1, -1, -1 };
-static pid_t riopid = 0;
 static void (*handler[LASTEvent]) (XEvent *) = {
 	[ButtonPress] = buttonpress,
 	[ClientMessage] = clientmessage,
@@ -354,6 +348,7 @@ static xcb_connection_t *xcon;
 
 #include "lib/ipc/ipc.h"
 #include "lib/ipc/util.h"
+#include "lib/rio.h"
 
 /* configuration, allows nested code to access above variables */
 #include "config.h"
@@ -367,6 +362,7 @@ static xcb_connection_t *xcon;
 
 #include "lib/ewmh.c"
 #include "lib/attach.c"
+#include "lib/rio.c"
 
 struct Pertag {
 	unsigned int curtag, prevtag; /* current and previous tag */
@@ -2447,105 +2443,6 @@ restack(Monitor *m)
 	}
 	XSync(dpy, False);
 	while (XCheckMaskEvent(dpy, EnterWindowMask, &ev));
-}
-
-int
-riodraw(Client *c, const char slopstyle[])
-{
-	int i;
-	char str[100];
-	char strout[100];
-	char tmpstring[30] = {0};
-	char slopcmd[100] = "slop -f x%xx%yx%wx%hx ";
-	int firstchar = 0;
-	int counter = 0;
-
-	strcat(slopcmd, slopstyle);
-	FILE *fp = popen(slopcmd, "r");
-
-	while (fgets(str, 100, fp) != NULL)
-		strcat(strout, str);
-
-	pclose(fp);
-
-	if (strlen(strout) < 6)
-		return 0;
-
-	for (i = 0; i < strlen(strout); i++){
-		if (!firstchar) {
-			if (strout[i] == 'x')
-				firstchar = 1;
-			continue;
-		}
-
-		if (strout[i] != 'x')
-			tmpstring[strlen(tmpstring)] = strout[i];
-		else {
-			riodimensions[counter] = atoi(tmpstring);
-			counter++;
-			memset(tmpstring,0,strlen(tmpstring));
-		}
-	}
-
-	if (riodimensions[0] <= -40 || riodimensions[1] <= -40 || riodimensions[2] <= 50 || riodimensions[3] <= 50) {
-		riodimensions[3] = -1;
-		return 0;
-	}
-
-	if (c) {
-		rioposition(c, riodimensions[0], riodimensions[1], riodimensions[2], riodimensions[3]);
-		return 0;
-	}
-
-	return 1;
-}
-
-void
-rioposition(Client *c, int x, int y, int w, int h)
-{
-	Monitor *m;
-	if ((m = recttomon(x, y, w, h)) && m != c->mon) {
-		detach(c);
-		detachstack(c);
-		arrange(c->mon);
-		c->mon = m;
-		c->tags = m->tagset[m->seltags];
-		attach(c);
-		attachstack(c);
-		selmon = m;
-		focus(c);
-	}
-
-	c->isfloating = 1;
-	if (riodraw_borders)
-		resizeclient(c, x, y, w - (c->bw * 2), h - (c->bw * 2));
-	else
-		resizeclient(c, x - c->bw, y - c->bw, w, h);
-	drawbar(c->mon);
-	arrange(c->mon);
-
-	riodimensions[3] = -1;
-	riopid = 0;
-}
-
-/* drag out an area using slop and resize the selected window to it */
-void
-rioresize(const Arg *arg)
-{
-	Client *c = (arg && arg->v ? (Client*)arg->v : selmon->sel);
-	if (c)
-		riodraw(c, slopresizestyle);
-}
-
-/* spawn a new window and drag out an area using slop to postiion it */
-void
-riospawn(const Arg *arg)
-{
-	if (riodraw_spawnasync) {
-		riopid = spawncmd(arg);
-		riodraw(NULL, slopspawnstyle);
-	} else if (riodraw(NULL, slopspawnstyle))
-		riopid = spawncmd(arg);
 }
 
 void
