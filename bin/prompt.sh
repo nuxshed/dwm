714e92735c8b9a204acbeab56b16228e9d003ccf
nuxsh <nuxshed@gmail.com>
2021-08-31 20:43:48 +0530

    improved dwmc prompt remove ipccommands file

--- bin/prompt.sh
+++ bin/prompt.sh
@@ -1,3 +1,39 @@
 #!/bin/sh
 
-dwmc run_command $(dmenu -fn "Fira Code Nerd Font:size=8" -nb "#1d2021" -nf "#ddc7a1" -sb "#89b482" -sf "#1d2021" < "$HOME/projects/dwm/ipccommands")
+ipccommands=$(cat<<EOF
+view
+toggleview
+tag
+toggletag
+tagmon
+focusmon
+focusstack
+zoom
+incnmaster
+killclient
+togglefloating
+setattachmode
+setstatus
+setmfact
+setlayoutsafe
+quit
+floatpos
+defaultgaps
+incrgaps
+incrigaps
+incrogaps
+incrihgaps
+incrivgaps
+incrohgaps
+incrovgaps
+incnmaster
+rioresize
+togglebar
+togglefullscreen
+togglefakefullscreen
+togglefloating
+togglegaps
+EOF
+)
+
+dwmc run_command $(echo "$ipccommands" | dmenu -fn "Fira Code Nerd Font:size=8" -nb "#1d2021" -nf "#ddc7a1" -sb "#89b482" -sf "#1d2021")
